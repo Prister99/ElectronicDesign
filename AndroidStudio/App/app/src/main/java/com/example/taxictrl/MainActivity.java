cddcd6363babee53533b1139aa26869642dece25
camilosalas99 <camiloandress@uninorte.edu.co>
2021-04-09 00:53:47 -0500

    App envia socketUDP

--- AndroidStudio/App/app/src/main/java/com/example/taxictrl/MainActivity.java
+++ AndroidStudio/App/app/src/main/java/com/example/taxictrl/MainActivity.java
@@ -13,14 +13,17 @@ import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.media.audiofx.BassBoost;
 import android.os.Bundle;
 import android.os.Looper;
 import android.provider.Settings;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -33,116 +36,94 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     //Initialize variable
-    Button btLocation;
+    Switch swLocation;
     TextView lat,lng,tms;
     FusedLocationProviderClient fusedLocationProviderClient;
-
+    Boolean SWST;
+    String IP= "191.108.168.10";
+    Integer PORT= 13550;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //Assign variable
-        btLocation = findViewById(R.id.bt_location);
-        lat = findViewById(R.id.text_lat);
-        lng = findViewById(R.id.text_lng);
-        tms = findViewById(R.id.text_tms);
+        swLocation = (Switch) findViewById(R.id.sw_location);
+        lat = (TextView) findViewById(R.id.text_lat);
+        lng = (TextView) findViewById(R.id.text_lng);
+        tms = (TextView) findViewById(R.id.text_tms);
 
         //Initialize fusedLocationProviderClient
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
-        btLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //check permission
-                if (ActivityCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                        && ActivityCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    //When permissions granted
-                    getCurrentLocation();
-                }else {
-                    //When permission denied
-                    //Request permission
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},100);
-                }
-            }
-        });
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void validarSW(View view) {
         //Check condition
-        if (requestCode == 100 && grantResults.length > 0 && (grantResults[0] + grantResults[1]
-                == PackageManager.PERMISSION_GRANTED)){
-            //When permission granted
-            //Call method
+        SWST = swLocation.isChecked();
+        if (ActivityCompat.checkSelfPermission(MainActivity.this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && SWST){
+
             getCurrentLocation();
+
         }else{
-            //When permission denied
-            Toast.makeText(getApplicationContext(),"Permission denied.", Toast.LENGTH_SHORT).show();
+
+            //When permission is denied
+            //Request permission
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},100);
         }
     }
-
     @SuppressLint("MissingPermission")
     private void getCurrentLocation() {
         //Initialize location manager
         LocationManager locationManager = (LocationManager) getSystemService(
-                Context.LOCATION_SERVICE
-        );
-        //check condition
-        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
-            //When location service is enabled  get last location
-            fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-                @Override
-                public void onComplete(@NonNull Task<Location> task) {
-                    //Initialize location
-                    Location location = task.getResult();
-                    //check condition
-                    if (location != null) {
-                        //Set Lat on TextView
-                        lat.setText(String.valueOf(location.getLatitude()));
-                        //Set Long on TextView
-                        lng.setText(String.valueOf(location.getLongitude()));
-                        //Set Tmstmp on TextView
-                        tms.setText(String.valueOf(location.getTime()));
-                        //Initialize location request
-                    }else {
-                        //When location result is null
-                        //Initialize location request
-                        LocationRequest locationRequest = new LocationRequest()
-                                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                                .setInterval(5000)
-                                .setFastestInterval(1000);
-                        //Initialize location callback
-                        LocationCallback locationCallback = new LocationCallback() {
-                            @Override
-                            public void onLocationResult(LocationResult locationResult) {
-                                //Initialize location
-                                Location location1 = locationResult.getLastLocation();
-                                //Set Lat on TextView
-                                lat.setText(String.valueOf(location1.getLatitude()));
-                                //Set Long on TextView
-                                lng.setText(String.valueOf(location1.getLongitude()));
-                                //Set Tmstmp on TextView
-                                tms.setText(String.valueOf(location1.getTime()));
-                            }
-                        };
-                        //Request location updates
-                        fusedLocationProviderClient.requestLocationUpdates(locationRequest,
-                                locationCallback, Looper.myLooper());
-                    }
+                Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                //Set Lat on TextView
+                lat.setText(String.valueOf(location.getLatitude()));
+                //Set Long on TextView
+                lng.setText(String.valueOf(location.getLongitude()));
+                //Set Tmstmp on TextView
+                long time = location.getTime();
+                Date date =new Date(time);
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                tms.setText(sdf.format(date));
+                SendData();
+                System.out.println("dsjfks");
+            }
+
+        });
+    }
+    public void SendData(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    DatagramSocket udpSocket = new DatagramSocket(PORT);
+                    InetAddress serverAddr = InetAddress.getByName(IP);
+                    byte[] buf = ("'"+lng.getText().toString()+"', '"+lat.getText().toString()+"', '"+tms.getText().toString()+"'").getBytes();
+                    DatagramPacket packet = new DatagramPacket(buf, buf.length,serverAddr, PORT);
+                    udpSocket.send(packet);
+                    udpSocket.close();
+                } catch (SocketException e) {
+                    Log.e("Udp:", "Socket Error:", e);
+                } catch (IOException e) {
+                    Log.e("Udp Send:", "IO Error:", e);
                 }
-            });
-        }else{
-            //When location service is not enabled
-            //Open location setting
-            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-        }
+            }
+        }).start();
     }
 }
